9dca210a6e4fe49b27a6529cb9e54668efdb5489
EternalProg <eternal.proglife@gmail.com>
2025-06-25 21:07:12 +0300

    Added processing using threads

--- examples/01_echo_server.cpp
+++ examples/01_echo_server.cpp
@@ -3,7 +3,12 @@
 #include "httpserver.hpp"
 
 int main() {
-  shttp::http_server server;
+  using namespace shttp;
+
+  HttpMethod method = HttpMethod::GET;
+  std::cout << HttpMethod::to_string(method) << '\n';
+
+  http_server server;
   // We can just create http_server and run it. The default behaviout is send
   // Request as Respond. So it just echo server by default
 
@@ -13,21 +18,21 @@ int main() {
     // must accept only 1 parameter HttpRequest and return HttpResponse
 
     server.setRequestHandler(
-        [](const shttp::HttpRequest& req) -> shttp::HttpResponse {
+        [](const HttpRequest& req) -> HttpResponse {
           std::cout << "Custom handler: " << req << '\n';
           return "It's custom handler:" + req ;
         });
   */
 
   // run function return HttpServerResult struct as the result
-  // We can check for error using this structure and if there's one get the error message
+  // We can check for error using this structure and if there's one get the
+  // error message
   auto result = server.run();
 
-  if(result.status != shttp::HttpServerStatus::OK) {
+  if (result.status != HttpServerStatus::OK) {
     std::cerr << "Server failed to run: " << result.error_message << '\n';
     return 1;
   }
-
 
   return 0;
 }
