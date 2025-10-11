65bd69035cdf1391e2ca0fb07ed095e7c71fb5fe
EternalProg <eternal.proglife@gmail.com>
2025-10-11 10:55:18 +0300

    Add utils directory and move there files

---
+++ utils/utils.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <fstream>
+#include <http_server_result.hpp>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <utils.hpp>
+#include <vector>
+
+namespace shttp::utils {}  // namespace shttp::utils
